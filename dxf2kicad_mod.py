71897d561a72df8936c6d244f33b821f542eb275
pandysong <pandy.song@gmail.com>
2018-08-21 21:45:14 +1000

    Refactor the code
    The code is refactored by using new created `kicad_mod_format.py`. The
    code is now easier to read and maintain.

--- dxf2kicad_mod.py
+++ dxf2kicad_mod.py
@@ -1,203 +1,144 @@
 # refer to http://pythonhosted.org/dxfgrabber/#
 # Note that there must not a line or shape overlapped
-import dxfgrabber
-import math
 import sys
-global debug
-global distance_error
-global body_head
-global body_end
-global fp_poly_head
-global fp_poly_end
+import math
+import functools
+from itertools import groupby
 
-debug = 0
-distance_error = 1e-2
-body_head = "\n\
-(module dxfgeneratedcopper (layer F.Cu) (tedit 0) \n\
-  (fp_text reference G*** (at 0 -4) (layer F.SilkS) hide \n\
-    (effects (font (thickness 0.2))) \n\
-  ) \n\
-  (fp_text value value (at 0 4) (layer F.SilkS) hide \n\
-    (effects (font (thickness 0.2))) \n\
-  ) "
+import dxfgrabber
 
-body_end = "\n)"
-
-fp_poly_head = "(fp_poly \
-  (pts "
-fp_poly_end_1 = " ) \n\
-	(layer "
-
-fp_poly_end_2 = " ) (width 0.001)) "
+import kicad_mod_format as kf
 
 
-def get_start_end_pts(entity):
+def _arc_point(center, radius, angle_degree):
+    '''
+    point defined by arc center,radius, and angel in degree
+    '''
+    return (center[0] + radius * math.cos(angle_degree/180*math.pi),
+            center[1] + radius * math.sin(angle_degree/180*math.pi))
+
+
+def _endpoints(entity):
+    '''
+    return a tuple of start and end points of the entity
+    '''
     if "LINE" == entity.dxftype:
-        if debug:
-            print >>sys.stderr, "LINE ", entity.start, entity.end
         return (entity.start, entity.end)
     elif "ARC" == entity.dxftype:
-        start = (entity.center[0] + entity.radius * math.cos(entity.startangle/180*math.pi),
-                 entity.center[1] + entity.radius * math.sin(entity.startangle/180*math.pi), 0)
-        end = (entity.center[0] + entity.radius * math.cos(entity.endangle/180*math.pi),
-               entity.center[1] + entity.radius * math.sin(entity.endangle/180*math.pi), 0)
-        if debug:
-            print >>sys.stderr, "ARC ", start, end
-        return (start, end)
+        return (_arc_point(entity.center, entity.radius, entity.start_angle),
+                _arc_point(entity.center, entity.radius, entity.end_angle))
     else:
-        print >>sys.stderr, "[Error]: Unexpceted dxftype ",  entity.dxftype
+        raise TypeError(
+            "[Error]: Unexpceted dxftype {}".format(entity.dxftype))
 
 
-def print_points(ety, direction):
+def _touched(p1, p2):
+    distance_error = 1e-2
+    return ((math.fabs(p1[0]-p2[0]) < distance_error) and
+            (math.fabs(p1[1]-p2[1]) < distance_error))
 
-    points = []
+
+def _points_in_entity(ety):
+
     if 'LINE' == ety.dxftype:
-        points = [ety.start, ety.end]
+        return [ety.start, ety.end]
     elif 'ARC' == ety.dxftype:
-        step = 1.0/ety.radius
-        angle = ety.startangle
+        if (ety.start_angle > ety.end_angle):
+            ety.end_angle += 360
 
-        if debug:
-            print >>sys.stderr, "angle", ety.startangle, ety.endangle
+        def angles(start_angle, end_angle, radius):
+            '''
+            yields descrete angles with step length defined by radius
+            '''
+            step = 1.0/ety.radius  # larger radius indicates small steps
+            angle = start_angle
+            while True:
+                yield angle
+                if (angle + step > ety.end_angle):
+                    yield end_angle
+                    break
+                else:
+                    angle += step
 
-        if (ety.startangle > ety.endangle):
-            ety.endangle += 360
-        while (1):
-            points.append((ety.center[0] + ety.radius * math.cos(angle/180*math.pi),
-                           ety.center[1] + ety.radius * math.sin(angle/180*math.pi)))
-            angle += step
-
-            if (angle > ety.endangle):
-                break
+        return [_arc_point(ety.center, ety.radius, a) for a in
+                angles(ety.start_angle, ety.end_angle, ety.radius)]
     else:
-        print "[Error]: Unexpceted dxftype ",  entity.dxftype
-
-    if direction == -1:
-        points.reverse()
-
-    for point in points:
-        # in KiCad Y axis has opposite direction
-        print "(xy ", point[0], " ", -point[1], ")"
+        raise TypeError(
+            "[Error]: Unexpceted dxftype {}".format(ety.dxftype))
 
 
-def start_new_shape():
-    global current_shape, points, point_to_close, pts_next
-    if len(not_processed_data) > 0:
-        current_shape = not_processed_data.pop()  # pick up one
-        points = get_start_end_pts(current_shape)
-        point_to_close = points[0]
-        pts_next = points[1]
+def fp_polys(layer, entities):
+    '''
+    yields fp_poly cmd in the layer of `entities`
+    '''
+    entities = list(entities)
 
-        if debug:
-            print >>sys.stderr, "starting point", point_to_close
-        print fp_poly_head
-        print_points(current_shape, 1)
+    def _points_next_to(next_start):
+        for e in entities:
+            start, end = _endpoints(e)
+            pts = _points_in_entity(e)
+            if _touched(next_start, start):
+                return pts, e
+            elif _touched(next_start, end):
+                pts.reverse()
+                return pts, e
+        return None, None
 
-
-#dxf = dxfgrabber.readfile("test4-R2000-2002.dxf")
-dxf = dxfgrabber.readfile(sys.argv[1])
-
-if debug:
-    print >>sys.stderr, "DXF version: {}", dxf.dxfversion
-header_var_count = len(dxf.header)  # dict of dxf header vars
-layer_count = len(dxf.layers)  # collection of layer definitions
-# dict like collection of block definitions
-block_definition_count = len(dxf.blocks)
-entity_count = len(dxf.entities)  # list like collection of entities
-if debug:
-    print >>sys.stderr, "Entity Count:"
-    print >>sys.stderr, entity_count
-
-layers = set([entity.layer for entity in dxf.entities])
-if debug:
-    print >>sys.stderr, layers
-
-
-print body_head
-
-
-for layer in layers:
-
-    not_processed_data = set(
-        [entity for entity in dxf.entities if entity.layer == layer])
-
-    pts_next = None
-    pts = None
-
-    start_new_shape()
-
-    if debug:
-        print >>sys.stderr, "Not Processed Shape: ", len(not_processed_data)
-
-    while (1):
-
-        if pts_next == None:
-            if debug:
-                print >>sys.stderr, "pts_next is None"
-            break  # stop
-        pts = pts_next
-
-        if debug:
-            print >>sys.stderr, "Searching entity which is connected with ", pts
-        # get_start_end_pts(current_shape)
-        matched_entity = None
-        pts_next = None
-        for entity in not_processed_data:
-            points = get_start_end_pts(entity)
-            x = points[0][0]
-            y = points[0][1]
-            if (math.fabs(x-pts[0]) < 5e-2) and (math.fabs(y-pts[1]) < 5e-2):
-                # matched
-                matched_entity = entity
-                direction = 1  # from start to end
-                pts_next = points[1]
-                if debug:
-                    print >>sys.stderr, "Got the Point", x, y
-                break
-            x = points[1][0]
-            y = points[1][1]
-            if (math.fabs(x-pts[0]) < distance_error) and (math.fabs(y-pts[1]) < distance_error):
-                # matched
-                matched_entity = entity
-                direction = -1  # from end to start
-                pts_next = points[0]
-                if debug:
-                    print >>sys.stderr, "Got the Point", x, y
-                break
-
-        if matched_entity == None:
-
-            if debug:
-                print >>sys.stderr, "No matching found, check if we could close the loop"
-
-            if (math.fabs(point_to_close[0]-pts[0]) < distance_error) and \
-                    (math.fabs(point_to_close[1]-pts[1]) < distance_error):
-                if debug:
-                    print >>sys.stderr, "shape closed at", pts
-                print "(xy ", pts[0], " ", -pts[1], ")"
-                print fp_poly_end_1, layer, fp_poly_end_2
-
-                # find next shape
-                start_new_shape()
-
-                if debug:
-                    print >>sys.stderr, "Not Processed Shape: ", len(
-                        not_processed_data)
+    def poly(e):
+        start, next_start = _endpoints(e)
+        yield [start]  # yield start points
+        while True:
+            pts, pts_e = _points_next_to(next_start)
+            if pts:
+                entities.remove(pts_e)  # remove from the set
+                yield pts  # yield a list of points
+                next_start = pts[-1]  # new start
             else:
-                print >>sys.stderr, "[Error] unconnected Point:", pts, " on layer", layer
-                print >>sys.stderr, "        there may be overlapped lines or arcs or unclosed shape, please double check the dxf file"
+                if _touched(next_start, start):
+                    return
+                else:
+                    raise ValueError('Unclosed shape at {}'.format(next_start))
 
-                break
-        else:
-            if debug:
-                print >>sys.stderr, "now print the line on,", matched_entity
-            print_points(matched_entity, direction)
-            if debug:
-                print >>sys.stderr, "removed from the set,", matched_entity
-            not_processed_data.remove(matched_entity)  # remove from the set
+    def polys():
+        while True:
+            if not entities:
+                return
+            e = entities.pop()  # pick up one
+            yield poly(e)  # yield an iterator which will yields points
 
-            if debug:
-                print >>sys.stderr, "Not Processed Shape: ", len(
-                    not_processed_data)
+    for p in polys():
+        poly_points = functools.reduce(lambda x, y: x+y, p)
+        # we may use *point, but since there might be more than 3 values in one
+        # point, we unpack it manually
+        yield kf.fp_poly(children=(kf.pts(children=(kf.xy(point[0], point[1])
+                                                    for point in poly_points)),
+                                   kf.layer(layer),
+                                   kf.width(0.001)))
 
-print body_end
+
+def _layer_entities(entities):
+    seq = list(entities)
+    seq.sort(key=lambda e: e.layer)
+    groups = groupby(seq, lambda e: e.layer)
+    return groups
+
+
+def cmds_from_entities(entities):
+    '''
+    get all cmd (in kicad_mod_format) from entities which is the entities
+    on all layers.
+    '''
+    return functools.reduce(lambda x, y: x+y,
+                            (list(fp_polys(layer, entities)) for (layer, entities) in
+                             _layer_entities(dxf.entities)))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('usage:\n'
+              '  save to a file:   python {} inputfile.dxf > outputfile.kicad_mod\n'
+              '  print to stdout:  python {} inputfile.dxf'.format(
+                  sys.argv[0], sys.argv[0]))
+    else:
+        dxf = dxfgrabber.readfile(sys.argv[1])
+        print(str(kf.Module('autogenerated', children=cmds_from_entities(dxf.entities))))
